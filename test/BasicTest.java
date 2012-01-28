7c510d8d1249d5e1fd0b1d34cf1c1a55744e3bc9
sakashushu <sakashushu@gmail.com>
2012-01-28 22:35:44 +0900

    win7でコミットし忘れ

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -22,4 +22,15 @@ public class BasicTest extends UnitTest {
 		assertNotNull(bob);
 		assertEquals("Bob", bob.fullname);
 	}
+	
+	@Test
+	public void tryConnectAsUser() {
+		// Create a new user and save it
+		new User("bob@gmail.com", "secret", "Bob").save();
+		
+		// Test
+		assertNotNull(User.connect("bob@gmail.com", "secret"));
+		assertNull(User.connect("bob@gmail.com", "badpassword"));
+		assertNull(User.connect("tom@gmail.com", "secret"));
+	}
 }
