21d4131a49a9dd2bbaa73bc59db986c453e0e071
sakashushu <sakashushu@gmail.com>
2012-02-01 16:00:33 +0900

    「キャプチャの設定」→「コメントフォームへのキャプチャ画像の追加」まで終了。

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
 import play.data.validation.Required;
+import play.libs.Codec;
+import play.libs.Images;
 import play.mvc.*;
 
 import java.util.*;
@@ -26,7 +29,8 @@ public class Application extends Controller {
 
     public static void show(Long id) {
     	Post post = Post.findById(id);
-    	render(post);
+    	String randamID = Codec.UUID();
+    	render(post, randamID);
     }
     
     public static void postComment(Long postId, @Required String author, @Required String content) {
@@ -37,5 +41,12 @@ public class Application extends Controller {
     	post.addComment(author, content);
     	flash.success("Thanks for posting %s", author);
     	show(postId);
+    }
+    
+    public static void captcha(String id) {
+    	Images.Captcha captcha = Images.captcha();
+    	String code = captcha.getText("#E4EAFD");
+    	Cache.set(id, code, "10min");
+    	renderBinary(captcha);
     }
 }
