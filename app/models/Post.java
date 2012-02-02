c1f90bc81d99e28f9fc6b2926c311dee0a186636
sakashushu <sakashushu@gmail.com>
2012-02-03 00:48:40 +0900

    「タグ機能のサポート」→「タグクラウド」まで終了

--- app/models/Post.java
+++ app/models/Post.java
@@ -3,13 +3,16 @@
  */
 package models;
 
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -32,15 +35,15 @@ public class Post extends Model {
 	
 	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
 	public List<Comment> comments;
-	
-	public Post(YabeUser author, String title, String content) {
-		this.comments = new ArrayList<Comment>();
-		this.author = author;
-		this.title = title;
-		this.content = content;
-		this.postedAt = new Date();
-	}
-	
+
+//	public Post(YabeUser author, String title, String content) {
+//		this.comments = new ArrayList<Comment>();
+//		this.author = author;
+//		this.title = title;
+//		this.content = content;
+//		this.postedAt = new Date();
+//	}
+//	
 	public Post addComment(String author, String content) {
 		Comment newComment = new Comment(this, author, content);
 		this.comments.add(newComment);
@@ -54,5 +57,36 @@ public class Post extends Model {
 	
 	public Post next() {
 		return Post.find("postedAt > ? order by postedAt asc", postedAt).first();
+	}
+
+	@ManyToMany(cascade=CascadeType.PERSIST)
+	public Set<Tag> tags;
+	
+	public Post(YabeUser author, String title, String content) {
+		this.comments = new ArrayList<Comment>();
+		this.tags = new TreeSet<Tag>();
+		this.author = author;
+		this.title = title;
+		this.content = content;
+		this.postedAt = new Date();
+	}
+	
+	public Post tagItWith(String name) {
+		tags.add(Tag.findOrCreateByName(name));
+		return this;
+	}
+	
+	public static List<Post> findTaggedWith(String tag) {
+		return Post.find(
+			"select distinct p from Post p join p.tags as t where t.name = ?", tag
+		).fetch();
+	}
+	
+	public static List<Post> findTaggedWith(String... tags) {
+		return Post.find(
+			"select distinct p from Post p join p.tags as t " +
+		    "where t.name in (:tags) group by p.id, p.author, p.title, " +
+			"p.content, p.postedAt having count(t.id) = :size "
+		).bind("tags", tags).bind("size", tags.length).fetch();
 	}
 }
