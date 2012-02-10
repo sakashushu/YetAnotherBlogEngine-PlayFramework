8fde13cb8e8c2cc82f48ed903ced1d6a8b1abec8
sakashushu <sakashushu@gmail.com>
2012-02-10 10:55:27 +0900

    「カスタムエディタの作成」→「‘新規投稿作成’ 画面」まで終了

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.Post;
+import models.Tag;
 import models.YabeUser;
 import play.mvc.Before;
 import play.mvc.Controller;
@@ -23,5 +24,29 @@ public class Admin extends Controller {
 		String user = Security.connected();
 		List<Post> posts = Post.find("author.email", user).fetch();
 		render(posts);
+	}
+	
+	public static void form() {
+		render();
+	}
+	
+	public static void save(String title, String content, String tags) {
+		// Create post
+		YabeUser author = YabeUser.find("byEmail", Security.connected()).first();
+		Post post = new Post(author, title, content);
+		// Set tags list
+		for(String tag : tags.split("\\s+")) {
+			if(tag.trim().length() > 0) {
+				post.tags.add(Tag.findOrCreateByName(tag));
+			}
+		}
+		// Validate
+		validation.valid(post);
+		if(validation.hasErrors()) {
+			render("@form", post);
+		}
+		// Save
+		post.save();
+		index();
 	}
 }
